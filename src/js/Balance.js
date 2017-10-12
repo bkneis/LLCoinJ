3bcfdfc6cf6c11bd67fba03a196aea4c310b9e3a
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-12 21:13:03 +0100

    COmment out getview for now

--- src/js/Balance.js
+++ src/js/Balance.js
@@ -1,18 +1,18 @@
 var Balance = {
-    getView: function (vendor) {
-        web3.eth.getAccounts(function(error, accounts) {
-            var viewInstance;
-            var account = accounts[0];
-            App.contracts[vendor].deployed().then(function (instance) {
-                viewInstance = instance;
-                return viewInstance.getView.call({ from: account });
-            }).then(function (view) {
-                $(el).clear().append(view);
-            }).catch(function (err) {
-                console.log(err.message);
-            });
-        });
-    },
+    // getView: function (vendor) {
+    //     web3.eth.getAccounts(function(error, accounts) {
+    //         var viewInstance;
+    //         var account = accounts[0];
+    //         App.contracts[vendor].deployed().then(function (instance) {
+    //             viewInstance = instance;
+    //             return viewInstance.getView.call({ from: account });
+    //         }).then(function (view) {
+    //             $(el).clear().append(view);
+    //         }).catch(function (err) {
+    //             console.log(err.message);
+    //         });
+    //     });
+    // },
     get: function (vendor, callback) {
         web3.eth.getAccounts(function(error, accounts) {
             var balanceInstance;
